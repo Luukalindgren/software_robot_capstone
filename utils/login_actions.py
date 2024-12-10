5aa68328de196b3aa43b4b890e0bd1f53e69636a
luukalindgren <luslin@utu.fi>
2024-12-10 21:55:03 +0200

    Debugging, increasing wait times

--- utils/login_actions.py
+++ utils/login_actions.py
@@ -10,12 +10,14 @@ def login_to_spåt(driver):
     password = os.getenv("SPAT_PASSWORD")
     url = os.getenv("SPAT_URL")
 
+    print("Logging in to Spåt with username:", username)
+
     try:
         driver.get(url)
         print("Website title: ", driver.title)
 
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "root")))
-        email_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email")))
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "root")))
+        email_input = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.NAME, "email")))
         password_input = driver.find_element(By.NAME, "password")
 
         email_input.clear()
@@ -23,12 +25,12 @@ def login_to_spåt(driver):
         password_input.clear()
         password_input.send_keys(password)
 
-        login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+        login_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
         login_button.click()
 
         WebDriverWait(driver, 20).until(EC.url_changes(url))
 
-        sessions_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "button-sessions")))
+        sessions_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "button-sessions")))
         sessions_button.click()
 
     except Exception as e:

74faa7ae9e3821fba30a7dc12fe8befdc60afcfb
luukalindgren <luslin@utu.fi>
2024-12-14 14:58:58 +0200

    Revert "Chromium tests"
    This reverts commit 7a75799117cab97ef8d0c5b1c6c404cebbd55c50.

--- utils/browser_setup.py
+++ utils/browser_setup.py
@@ -2,13 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 import os
+import chromedriver_autoinstaller
 
 def create_browser():
     """Create and return a browser instance with specified download settings."""
-    
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+    chromedriver_autoinstaller.install()
 
     download_folder = os.path.join(os.getcwd(), "temp")
 
@@ -21,7 +21,6 @@ def create_browser():
     options.add_argument("--window-size=1920x1080")
     options.add_argument("--disable-gpu")  # For better performance
     options.add_argument("--no-sandbox")
-    options.add_argument("--ignore-certifiate-errors")
     options.add_argument("--disable-dev-shm-usage")
 
     # options.add_argument("--enable-logging")
@@ -38,6 +37,7 @@ def create_browser():
 
     print("Browser options set for download directory:", prefs["download.default_directory"])
 
-    driver = webdriver.Chrome(service=chrome_service, options=options)
+    #service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=options)
 
     return driver

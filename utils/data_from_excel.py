3ce0f7ec8a096d21e681f7ba29e7111d0dce1a9a
Luuka Lindgren <luslin@utu.fi>
2025-01-16 10:40:57 +0200

    Copied updates from Iiro's code

--- utils/data_from_excel.py
+++ utils/data_from_excel.py
@@ -1,4 +1,7 @@
+
+###############################################
 ## COPIED FROM IIRO 'sp-talytics-task1' REPO
+###############################################
 
 from openpyxl import load_workbook
 
@@ -22,7 +25,7 @@ def get_user_data(full_path):
     #Next we will pull the data from the excel file and format it to the schema.
 
     #Define data_file as the path to file !!This must be made generic so that it can handle multiple data files!!
-    data_file = full_path #For now, uses a single file.
+    data_file = full_path 
 
     #Load entire workbook aka all work sheets in the excel file
     workbook = load_workbook(data_file)
@@ -35,7 +38,9 @@ def get_user_data(full_path):
         #create the first instance of the user data dictionary and give the user an id.
         user_data.append({
             'user_id' : sheet.title,
-            'speed_data' : []
+            'speed_data' : [],
+            'acceleration': 0,
+            'deceleration': 0
         })
         #col_names contains the titles of the needed data, this allows the iteration of those columns
         col_names = {}
@@ -44,8 +49,15 @@ def get_user_data(full_path):
         #Iterate through the whole sheet to create a dictionary with column names and their column number.
         #E.g speedzone is column 8, this will be used later to get all relevant data.
         for COL in active_sheet.iter_cols(1, active_sheet.max_column):
-            col_names[COL[0].value] = current
+            #print(COL[0].value)
+            if(COL[0].value == None and current > 5):
+                col_names["None2"] = current
+            elif(COL[0].value == "Percentage (%)" and current > 10):
+                col_names["Percentage2"] = current
+            else:
+                col_names[COL[0].value] = current
             current += 1
+        #print(col_names)
 
         #Now iterate through the table rows to find all rows that contain data from the chosen column.
         for row_cells in active_sheet.iter_rows(min_row=2, max_row=5):
@@ -74,19 +86,32 @@ def get_user_data(full_path):
         iteration = 0
         for row_cells in active_sheet.iter_rows(min_row=2, max_row=5):
             if(row_cells[col_names['Percentage (%)']].value != None):
-                user_data[worksheet_count]["speed_data"][iteration]["time_percentage"] = row_cells[col_names['Percentage (%)']].value
+                user_data[worksheet_count]["speed_data"][iteration]["time_percentage"] = float(row_cells[col_names['Percentage (%)']].value)
                 iteration += 1
         #total_distance
         iteration = 0
         for row_cells in active_sheet.iter_rows(min_row=2, max_row=5):
             if(row_cells[col_names['Total Distance (m)']].value != None):
-                user_data[worksheet_count]["speed_data"][iteration]["total_distance"] = row_cells[col_names['Total Distance (m)']].value
+                user_data[worksheet_count]["speed_data"][iteration]["total_distance"] = float(row_cells[col_names['Total Distance (m)']].value)
                 iteration += 1
         #distance_percentage
         iteration = 0
         for row_cells in active_sheet.iter_rows(min_row=2, max_row=5):
-            if(row_cells[col_names['Percentage (%)']].value != None):
-                user_data[worksheet_count]["speed_data"][iteration]["distance_percentage"] = row_cells[col_names['Percentage (%)']].value
+            if(row_cells[col_names['Percentage2']].value != None):
+                user_data[worksheet_count]["speed_data"][iteration]["distance_percentage"] = float(row_cells[col_names['Percentage2']].value)
                 iteration += 1
+        #acceleration
+        iteration = 0
+        for row_cells in active_sheet.iter_rows(min_row=12, max_row=13):
+            if(row_cells[col_names['Total Time']].value != None):
+                user_data[worksheet_count]["acceleration"] = int(row_cells[col_names['Total Time']].value)
+                iteration += 1
+        #deceleration
+        iteration = 0
+        for row_cells in active_sheet.iter_rows(min_row=12, max_row=13):
+            if(row_cells[col_names['Percentage (%)']].value != None):
+                user_data[worksheet_count]['deceleration'] = row_cells[col_names['Percentage (%)']].value
+                iteration += 1
+
         worksheet_count += 1
     return(user_data)
